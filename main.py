324dfe67de76eacbfb6374affbcd228cd49aa39d
Marek Jarczewski <marek.jarczewski81@gmail.com>
2023-05-30 22:42:20 +0200

    Dodanie tresci zadania

---
+++ main.py
@@ -0,0 +1,4 @@
+# zadanie 1: stwórz liste zawierajaca 5 owoców, a nastepnie
+# za pomocą pętli for i nowej listy odwróć każdyz tych owoców
+#     arbuz => zubra
+# wynik (w nowej liście) wyprintuj na ekranie
