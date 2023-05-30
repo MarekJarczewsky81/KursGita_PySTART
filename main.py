68ed498d0ecdb61f364cf21e8358333e99982351
Marek Jarczewski <marek.jarczewski81@gmail.com>
2023-05-31 00:12:40 +0200

    new list with reversed strings

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 # zadanie 1: stwórz liste zawierajaca 5 owoców, a nastepnie
-# za pomocą pętli for i nowej listy odwróć każdyz tych owoców
+# za pomocą pętli for i nowej listy odwróć każdy z tych owoców
 #     arbuz => zubra
-# wynik (w nowej liście) wyprintuj na ekranie
+# wynik (w nowej l   iście) wyprintuj na ekranie
+
+fruits = ['apple', 'lemon', 'mango', 'kiwi', 'cherry']
+new_list = []
+for fruit in fruits:
+    new_list.append(fruit[::-1])
+
+print(new_list)
